4589eea26532e8118a7545666e071a96b5f93d64
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-24 15:18:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
 def oxford_comma(array)
   case array.length
     when 1
-      return array.to_s 
-  
-  last = array.pop
-  string = array.join(", ")
-  string << ", and #{last}"
-  return string
+      return array.to_s
+    when 2
+      return array.join(" and ")
+    else
+      last = array.pop
+      string = array.join(", ")
+      string << ", and #{last}"
+      return string
+    end
 end
